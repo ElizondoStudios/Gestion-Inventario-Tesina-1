0a545349c8f530767d7d5b5a606f25896aaf7674
Elizondo Studios <elizondostudios@gmail.com>
2026-01-14 16:19:04 -0600

    Agregar tablas faltantes al DB Context

--- API/Data/AppDBContext.cs
+++ API/Data/AppDBContext.cs
@@ -4,4 +4,15 @@ using API.Entities;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<PerfilPuesto> PerfilesPuesto { get; set; }
+    public DbSet<Modulo> Modulos { get; set; }
+    public DbSet<ModulosCategoria> ModulosCategorias { get; set; }
+    public DbSet<NivelesAcceso> NivelesAcceso { get; set; }
+    public DbSet<ModulosAcceso> ModulosAcceso { get; set; }
+    public DbSet<Sucursal> Sucursales { get; set; }
+    public DbSet<SucursalesInventario> SucursalesInventario { get; set; }
+    public DbSet<Inventario> Inventario { get; set; }
+    public DbSet<Unidad> Unidades { get; set; }
+    public DbSet<LogInventario> LogInventario { get; set; }
+    public DbSet<UsuarioSucursal> UsuariosSucursales { get; set; }
 }
