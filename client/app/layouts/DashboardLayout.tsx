aacfb29ce48573c499d15893551076f76330ca4c
Elizondo Studios <elizondostudios@gmail.com>
2026-02-10 17:41:33 -0600

    Validar las rutas

--- client/app/layouts/DashboardLayout.tsx
+++ client/app/layouts/DashboardLayout.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sidebar from '~/components/Sidebar'
 import Header from '~/components/Header'
+import { auth } from 'services/auth'
+import { useNavigate } from 'react-router'
 
 export default function DashboardLayout({children}: {children: React.ReactNode}) {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    // Validar que el usuario tenga sesión iniciada
+    const authToken= auth.getToken();
+    if(authToken===null){
+      navigate("/login")
+    }
+  }, [])
+  
   return (
     <>
       <main className='min-h-dvh w-full flex'>
